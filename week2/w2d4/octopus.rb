069d915696f50e0c4a42ace419e9bb6842cbe443
Jeffrey Fan <jeffreyfan@Jeffreys-MacBook-Air.local>
2016-11-23 22:29:15 -0800

    finished octopus

--- week2/w2d4/octopus.rb
+++ week2/w2d4/octopus.rb
@@ -56,9 +56,22 @@ def clever_octopus(array)
 end
 
 def slow_dance(direction, tiles_array)
-
+  tiles_array.each_with_index do |tile, idx|
+    return idx if tile == direction
+  end
 end
 
-def fast_dance(direction, tiles_hash)
+tiles_hash = {
+    "up" => 0,
+    "right-up" => 1,
+    "right"=> 2,
+    "right-down" => 3,
+    "down" => 4,
+    "left-down" => 5,
+    "left" => 6,
+    "left-up" => 7
+}
 
+def fast_dance(direction, tiles_hash)
+  tiles_hash[direction]
 end
